bb64ef701879afd2f4fe656ebe72ef6ad1f5d293
ziaqut <154807487+ziaqut@users.noreply.github.com>
2024-08-31 23:56:17 +1000

    test aritmatics

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    c = a + b
+    return c
+
+def subtract(a, b):
+    if (a  >  b):
+        c= a -b
+    else:
+        c = b - a
+    return c
+
---
+++ TestCalc.py
@@ -0,0 +1,7 @@
+import Calculator as cal
+
+a = int(input("Enter the first number a: "))
+b = int(input("Enter the 2nd number b: "))
+
+print("Addition(a, b):", cal.add(a, b))
+print("Subtract(a, b):", cal.subtract(a, b))
